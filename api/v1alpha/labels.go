8a5bbf71a3827d69c5fa15caf3805bb06031822b
Joshua Reese <joshlreese@gmail.com>
2025-03-07 07:41:00 -0600

    Use correct constant for label prefix.

--- api/v1alpha/labels.go
+++ api/v1alpha/labels.go
@@ -3,6 +3,6 @@ package v1alpha
 const (
 	LabelNamespace = "compute.datumapis.com"
 
-	WorkloadUIDLabel           = AnnotationNamespace + "/workload-uid"
-	WorkloadDeploymentUIDLabel = AnnotationNamespace + "/workload-deployment-uid"
+	WorkloadUIDLabel           = LabelNamespace + "/workload-uid"
+	WorkloadDeploymentUIDLabel = LabelNamespace + "/workload-deployment-uid"
 )
